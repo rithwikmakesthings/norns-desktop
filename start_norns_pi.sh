d3e4627048b536e05dc2e7ffcd5b9ef6970100c2
Rithwik Sathyan <12883933+rithwikmakesthings@users.noreply.github.com>
2023-09-19 19:46:56 +1000

    Update start_norns_pi.sh

--- start_norns_pi.sh
+++ start_norns_pi.sh
@@ -1,18 +1,19 @@
 #/bin/bash
 
 # start screen
-Xvfb :0 -screen 0 1280x640x16 -fbdir /tmp &
+#Xvfb :0 -screen 0 1280x640x16 -fbdir /tmp &
 sleep 1
 cd /home/we/norns-desktop
 LOGGER=info /usr/local/go/bin/go run oled-server.go -window-name 'matron' -port 8889 &
-sleep 1
+#sleep 1
 
 # start jack
 export JACK_NO_START_SERVER=1
 export JACK_NO_AUDIO_RESERVATION=1
 export JACK_AUDIO=$(aplay -l | head -n1 | grep USB | sed 's/:/ /g' | awk '{print $2}')
-/usr/bin/jackd -R -P 95 -d alsa -P hw:$JACK_AUDIO -i 2 & # playback only
+#/usr/bin/jackd -R -P 95 -d alsa -P hw:$JACK_AUDIO -i 2 & # playback only
 #/usr/bin/jackd -R -P 95 -d alsa -d hw:$JACK_AUDIO -p 1024 & # 2x2 USB interface
+/usr/bin/jackd -t 2000 -R -P 95 -d alsa -d hw:pisound -r 48000 &
 
 sleep 1
 
@@ -23,8 +24,11 @@ sleep 1
 /home/we/norns/build/ws-wrapper/ws-wrapper 'ws://*:5556' /usr/local/bin/sclang -i maiden &
 
 # start matron 
-export DISPLAY=:0
+#export DISPLAY=:0
+#xrandr --output DSI-1 --mode 800x480 --scale 1x1
 /home/we/norns/build/ws-wrapper/ws-wrapper 'ws://*:5555' /home/we/norns/build/matron/matron &
+#xprop -name 'matron' -format _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS 2 
+#wmctrl -r ':ACTIVE:' -b toggle,fullscreen
 
 # start maiden
 cd /home/we/maiden && ./maiden server --app ./app/build --data ~/dust --doc ~/norns/doc &
