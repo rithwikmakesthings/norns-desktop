356e046fa0636d91f319746a06d01cb454017aeb
Rithwik Sathyan <12883933+rithwikmakesthings@users.noreply.github.com>
2023-09-24 11:51:44 +1000

    Update start_norns_pi.sh

--- start_norns_pi.sh
+++ start_norns_pi.sh
@@ -3,7 +3,7 @@
 # start screen
 #Xvfb :0 -screen 0 1280x640x16 -fbdir /tmp &
 sleep 1
-cd /home/we/norns-desktop
+cd $HOME/norns-desktop
 LOGGER=info /usr/local/go/bin/go run oled-server.go -window-name 'matron' -port 8889 &
 #sleep 1
 
@@ -18,25 +18,25 @@ export JACK_AUDIO=$(aplay -l | head -n1 | grep USB | sed 's/:/ /g' | awk '{print
 sleep 1
 
 # start crone
-/home/we/norns/build/crone/crone &
+$HOME/norns/build/crone/crone &
 
 # start sc
-/home/we/norns/build/ws-wrapper/ws-wrapper 'ws://*:5556' /usr/local/bin/sclang -i maiden &
+$HOME/norns/build/ws-wrapper/ws-wrapper 'ws://*:5556' /usr/local/bin/sclang -i maiden &
 
 # start matron 
 #export DISPLAY=:0
 #xrandr --output DSI-1 --mode 800x480 --scale 1x1
-/home/we/norns/build/ws-wrapper/ws-wrapper 'ws://*:5555' /home/we/norns/build/matron/matron &
+$HOME/norns/build/ws-wrapper/ws-wrapper 'ws://*:5555' $HOME/norns/build/matron/matron &
 #xprop -name 'matron' -format _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS 2 
 #wmctrl -r ':ACTIVE:' -b toggle,fullscreen
 
 # start maiden
-cd /home/we/maiden && ./maiden server --app ./app/build --data ~/dust --doc ~/norns/doc &
+cd $HOME/maiden && ./maiden server --app ./app/build --data ~/dust --doc ~/norns/doc &
 
 # optional, start icecast
-# icecast2 -c /home/we/norns-desktop/icecast.xml &
+# icecast2 -c $HOME/norns-desktop/icecast.xml &
 # sleep 0.5
-# darkice -c /home/we/norns-desktop/darkice.cfg &
+# darkice -c $HOME/norns-desktop/darkice.cfg &
 # sleep 0.5
 # jack_connect crone:output_1 darkice:left
 # jack_connect crone:output_2 darkice:right
