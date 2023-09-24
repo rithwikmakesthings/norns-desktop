f1942bcd768af066ea2915f3573ca29817726802
Rithwik Sathyan <12883933+rithwikmakesthings@users.noreply.github.com>
2023-09-24 11:52:29 +1000

    Update start_norns.sh

--- start_norns.sh
+++ start_norns.sh
@@ -5,24 +5,24 @@ export JACK_NO_START_SERVER=1
 export JACK_NO_AUDIO_RESERVATION=1
 
 sudo /etc/init.d/dbus start
-sudo chown -R we:we /home/we/dust
+sudo chown -R we:we $HOME/dust
 Xvfb :0 -screen 0 1280x640x16 -fbdir /tmp &
 sleep 0.5
-cd /home/we/ && LOGGER=info /usr/local/go/bin/go run oled-server.go -window-name 'matron' -port 8889 &
+cd $HOME && LOGGER=info /usr/local/go/bin/go run oled-server.go -window-name 'matron' -port 8889 &
 sleep 0.5
 jackd -V
 $(cat /etc/jackdrc) &
 sleep 0.5
-/home/we/norns/build/crone/crone &
-cd /home/we/norns/sc
+$HOME/norns/build/crone/crone &
+cd $HOME/norns/sc
 sleep 0.5
-timeout 0.5 /home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5556 /usr/local/bin/sclang -i maiden
+timeout 0.5 $HOME/norns/build/ws-wrapper/ws-wrapper ws://*:5556 /usr/local/bin/sclang -i maiden
 sleep 0.5
-/home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5556 /usr/local/bin/sclang -i maiden &
+$HOME/norns/build/ws-wrapper/ws-wrapper ws://*:5556 /usr/local/bin/sclang -i maiden &
 sleep 0.5
-/home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5555 /home/we/norns/build/matron/matron &
+$HOME/norns/build/ws-wrapper/ws-wrapper ws://*:5555 $HOME/norns/build/matron/matron &
 sleep 0.5
-cd /home/we/maiden && ./maiden server --app ./app/build --data ~/dust --doc ~/norns/doc &
+cd $HOME/maiden && ./maiden server --app ./app/build --data ~/dust --doc ~/norns/doc &
 sleep 0.5
 icecast2 -c /etc/icecast2/icecast.xml &
 sleep 0.5
